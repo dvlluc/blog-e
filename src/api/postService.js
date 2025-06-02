19e0b6904d8f792caf94125a2cdbac53ce57e05e
Andrey Gritsuk <axa8xa8@gmail.com>
2025-06-02 10:57:57 +0300

    fix nodeRef

--- src/api/postService.js
+++ src/api/postService.js
@@ -2,8 +2,12 @@ import axios from "axios";
 import { createRef } from "react";
 
 export default class PostService {
-  static async getAll() {
-    const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts");
-    return data.map((post) => ({ ...post, nodeRef: createRef(null) }));
+  static async getAll(limit = 10, number = 1) {
+    return await axios.get("https://jsonplaceholder.typicode.com/posts", {
+      params: {
+        _limit: limit,
+        _page: number,
+      },
+    });
   }
 }
