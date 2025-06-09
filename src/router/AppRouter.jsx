a0edb49d7e909c77ab2752d743b64bb051a90788
Andrey Gritsuk <axa8xa8@gmail.com>
2025-06-09 11:50:19 +0300

    router some fixes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,40 +1,39 @@
 import { Navigate, Route, Routes, useRoutes } from "react-router-dom";
 import About from "../pages/About";
 import Posts from "../pages/Posts";
-// import PostIdPage from "../pages/Posts/PostIdPage";
-import Error from "../pages/Error";
 import PostIdPage from "../pages/PostIdPage";
+import Error from "../pages/Error";
 
 const AppRouter = () => {
-  // return (
-  //   <Routes>
-  //     <Route path="/posts" element={<Posts />} />
-  //     <Route path="/posts/:id" element={<PostIdPage />} />
-  //     <Route path="/about" element={<About />} />
-  //     <Route path="*" element={<Error />} />
-  //     {/* <Route path="*" element={<Navigate to="/posts" replace />} /> */}
-  //   </Routes>
-  // );
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/posts" replace/>} />
+      <Route path="/posts" element={<Posts />} />
+      <Route path="/posts/:id" element={<PostIdPage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
 
-  return useRoutes([
-    {
-      path: "posts",
-      element: <Posts />,
-    },
-    {
-      path: "posts:id",
-      element: <PostIdPage />,
-    },
-    {
-      path: "about",
-      element: <About />,
-    },
-    {
-      path: "*",
-      element: <Error />,
-      // element: <Navigate to="/posts" replace />,
-    },
-  ]);
+  // return useRoutes([
+  //   {
+  //     path: "posts",
+  //     element: <Posts />,
+  //   },
+  //   {
+  //     path: "posts:id",
+  //     element: <PostIdPage />,
+  //   },
+  //   {
+  //     path: "about",
+  //     element: <About />,
+  //   },
+  //   {
+  //     path: "*",
+  //     element: <Error />,
+  //     // element: <Navigate to="/posts" replace />,
+  //   },
+  // ]);
 };
 
 export default AppRouter;
