c0988641b99e437c8198bd5a0e44aaacab4b3b9d
Andrey Gritsuk <axa8xa8@gmail.com>
2025-06-09 14:04:55 +0300

    refactor routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/UI/Navbar/Navbar";
-import AppRouter from "./router/AppRouter";
+import AppRouter from "./components/AppRouter";
 import "./styles/App.css";
 
 const App = () => {
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,5 +1,5 @@
 import { useRoutes } from "react-router-dom";
-import { routes } from ".";
+import { routes } from "../router";
 
 const AppRouter = () => {
   return useRoutes(routes);
